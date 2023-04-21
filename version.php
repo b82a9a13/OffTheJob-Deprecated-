886848f2fead8f6389d56b64d016d7561aa7e482
b82a9a13 <85174237+b82a9a13@users.noreply.github.com>
2023-04-21 13:19:06 +0100

    b00a0a09

--- version.php
+++ version.php
@@ -24,5 +24,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_offthejob';
-$plugin->version = 189;
+$plugin->version = 190;
 $plugin->requires = 2016052314; // Moodle version
